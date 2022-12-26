5649a51073a7ad8fc6290399fff2273c6b8589e0
Niklas Gabriel <nikgab@gmx.de>
2022-12-26 14:42:18 +0100

    Previews integrated in Scroll bars

--- artikel.php
+++ artikel.php
@@ -7,7 +7,28 @@
 <body>
     <?php include("extra/menu.php");?>
     <div class="content">
-        <h1>ARTIKEL</h1>
+        <h2>MSD</h2>
+        <div class="scroll_preview">
+            <div class="scroll_content">
+                <?php include("previews/test.php");?>
+                <?php include("previews/test.php");?>
+                <?php include("previews/test.php");?>
+                <?php include("previews/test.php");?>
+                <?php include("previews/test.php");?>
+                <?php include("previews/test.php");?>
+            </div>
+        </div>
+        <h2>Test</h2>
+        <div class="scroll_preview">
+            <div class="scroll_content">
+                <?php include("previews/test.php");?>
+                <?php include("previews/test.php");?>
+                <?php include("previews/test.php");?>
+                <?php include("previews/test.php");?>
+                <?php include("previews/test.php");?>
+                <?php include("previews/test.php");?>
+            </div>
+        </div>
     </div>
 </body>
 </html>
---
+++ previews/test.php
@@ -0,0 +1,9 @@
+<div class="preview">
+    <a href="">
+        <img class="preview_img" src="previews/test.png" alt="">
+        <div class="preview_texts">
+            <p class="preview_header">Test</p>
+            <p class="preview_text">Stärke deine Kompetenzen im Weitwurf!</p>
+        </div>
+    </a>
+</div>
