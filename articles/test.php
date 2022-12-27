8f3b7551d343503c5b4460047a4ecd1611183b18
Niklas Gabriel <nikgab@gmx.de>
2022-12-27 09:46:25 +0100

    Srticle Style Elements

--- articles/test.php
+++ articles/test.php
@@ -1,4 +1,16 @@
 <div class="article">
-    <h1>Tetlein</h1>
-    <p>TEXT Text</p>
+    <h1>1.1 Header</h1>
+    <h2>1.1.1 Subheader</h2>
+    <p>Default Text</p>
+    <ol>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+    </ol>
+    <hr>
+    <h2>1.1.2 Subheader</h2>
 </div>
